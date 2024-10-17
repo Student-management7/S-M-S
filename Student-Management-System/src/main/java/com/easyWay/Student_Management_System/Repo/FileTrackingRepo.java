ea9f421b91791186e49528156dbea9613a22201a
Ashishgithubnew <ash0112255@gmail.com>
2024-10-17 11:56:42 +0530

    changes

--- Student-Management-System/src/main/java/com/easyWay/Student_Management_System/Repo/FileTrackingRepo.java
+++
@@ -1,9 +0,0 @@
-package com.easyWay.Student_Management_System.Repo;
-
-import com.easyWay.Student_Management_System.Entity.FileTracking;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.UUID;
-
-public interface FileTrackingRepo extends JpaRepository<FileTracking , UUID> {
-}

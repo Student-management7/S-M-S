b22f8aa78ed3820b6a95182ce872b7f1d687fc3a
Gauri <sharmagauri222002@gmail.com>
2025-01-18 21:53:07 +0530

    cors

--- Student-Management-System/src/main/java/com/easyWay/Student_Management_System/Config/WebConfig.java
+++ Student-Management-System/src/main/java/com/easyWay/Student_Management_System/Config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173","https://student-management-system-gui-3.onrender.com")
+                .allowedOrigins("http://localhost:5173","https://student-management-system-gui-3.onrender.com", "http://localhost:61955/")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);

cf390c1fcb341f71f34cf2f987e979a684cd4fbb
Ashishgithubnew <ash0112255@gmail.com>
2025-02-01 21:43:15 +0530

    changes for deploy

--- Student-Management-System/src/main/java/com/easyWay/Student_Management_System/Config/WebConfig.java
+++ Student-Management-System/src/main/java/com/easyWay/Student_Management_System/Config/WebConfig.java
@@ -2,6 +2,7 @@ package com.easyWay.Student_Management_System.Config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -14,5 +15,14 @@ public class WebConfig implements WebMvcConfigurer {
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
+    }
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+
+        registry.addViewController("/{spring:\\w+}").setViewName("forward:/index.html");
+        registry.addViewController("/**/{spring:\\w+}").setViewName("forward:/index.html");
+        registry.addViewController("/{spring:\\w+}/**{spring:?!(\\.js|\\.css|\\.ico|\\.json)$}")
+                .setViewName("forward:/index.html");
     }
 }

f93f15c85257ee545fdc27ee657b406daf479e0c
Ashish <ash0112255@gmail.com>
2025-08-16 15:55:12 +0530

    changes

--- Student-Management-System/src/main/java/com/easyWay/Student_Management_System/Entity/Users.java
+++ Student-Management-System/src/main/java/com/easyWay/Student_Management_System/Entity/Users.java
@@ -17,7 +17,7 @@ public class Users extends  BaseEntity{
     private String password;
     private String schoolCode;
     private String role;
-    private boolean isActive = true;
+    private boolean isActive = false;
 
     @Column(columnDefinition = "Text")
     private String permission ;
